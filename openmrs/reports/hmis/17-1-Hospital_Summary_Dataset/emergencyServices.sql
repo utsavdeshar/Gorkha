e27e18b4a4b8b9fb94f6b2a9fd692bf42356265a
utsavdeshar <utsavd@hotmail.com>
2021-03-23 15:53:39 +0545

    Update emergencyServices.sql
    Update Query according to Standard Dataset concept.

--- openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/emergencyServices.sql
+++ openmrs/reports/hmis/17-1-Hospital_Summary_Dataset/emergencyServices.sql
@@ -1,21 +1,21 @@
-SELECT
-  rag.name                                   AS ageGroup,
-  SUM(IF(person_details.gender = 'F', 1, 0)) AS Female,
-  SUM(IF(person_details.gender = 'M', 1, 0)) AS Male,
-  SUM(IF(person_details.gender = 'O', 1, 0)) AS Other
-FROM reporting_age_group rag
-  LEFT JOIN
-  (SELECT
-     birthdate,
-     gender,
-     date_started
-   FROM person AS p
-     JOIN visit
-       ON p.person_id = visit.patient_id AND cast(visit.date_started AS DATE) BETWEEN '#startDate#' AND '#endDate#'
-     JOIN visit_type vt ON visit.visit_type_id = vt.visit_type_id AND vt.name = 'Emergency') AS person_details
-    ON cast(date_started AS DATE) BETWEEN (DATE_ADD(
-      DATE_ADD(person_details.birthdate, INTERVAL rag.min_years YEAR), INTERVAL rag.min_days
-      DAY))
-  AND (DATE_ADD(DATE_ADD(person_details.birthdate, INTERVAL rag.max_years YEAR), INTERVAL rag.max_days DAY))
-WHERE rag.report_group_name = 'Emergency Service Reports'
-GROUP BY ageGroup;
+SELECT client_visits.age_group AS 'Age Group',
+IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'F', 1, 0))) as 'Total Clients Female',
+IF(client_visits.patient_id IS NULL, 0, SUM(IF(client_visits.patient_gender = 'M', 1, 0))) as 'Total Clients Male'
+FROM
+(SELECT DISTINCT patient.patient_id AS patient_id,
+observed_age_group.name AS age_group,
+observed_age_group.id as age_group_id,
+patient.date_created AS first_visit_date,
+person.gender AS patient_gender,
+observed_age_group.sort_order AS sort_order
+FROM visit
+INNER JOIN patient ON visit.patient_id = patient.patient_id AND DATE(visit.date_started) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) AND patient.voided = 0 AND visit.voided = 0
+INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+JOIN visit_type vt ON visit.visit_type_id = vt.visit_type_id AND vt.name in ( 'Emergency')
+
+RIGHT OUTER JOIN reporting_age_group AS observed_age_group ON
+DATE(visit.date_started) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+WHERE observed_age_group.report_group_name = 'Client Service Reports') AS client_visits
+GROUP BY client_visits.age_group
+ORDER BY client_visits.sort_order
